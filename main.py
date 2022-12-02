7dcde581a30a8972d71254e71493a6a753cd3da6
Sal888 <57865053+Sal888@users.noreply.github.com>
2022-12-02 10:28:37 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import base64
 import os
-from uuid import uuid4
+import shutil
 
 from flask import Flask, Response, render_template, request
 from werkzeug.utils import secure_filename
@@ -8,36 +9,61 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("home.html")
 
 
-@app.errorhandler(404)
-def not_found(e):
-    return render_template("404.html")
-
-
 @app.route("/upload", methods=["POST"])
-def file_upload():
-
+def upload():
     if request.method == "POST":
-
         f = request.files["file"]
         filename = secure_filename(f.filename)
 
         if not f.mimetype.endswith("pcap"):
             return Response("Bad file", status=400)
+
+        if os.path.exists("static/uploads"):
+            pass
         else:
+            os.mkdir("static/uploads")
+        final_path = os.path.join("static/uploads", filename)
+        f.save(final_path)
+        os.system(f"python3 scripts/pcap_analyser.py static/uploads/{filename}")
 
-            if os.path.exists("uploads"):
-                pass
-            else:
-                os.mkdir("uploads")
+        with open("tables/netcap_stats.html", "r") as f:
+            netcap_stats = f.read()
+        with open("tables/enum_images.html", "r") as f:
+            enum_images = f.read()
+        with open("tables/email_addrs.html", "r") as f:
+            email_addrs = f.read()
+        with open("tables/packet_count.html", "r") as f:
+            packet_count = f.read()
 
-            f.save(os.path.join("uploads", filename))
-            x = uuid4()
-            return f"https://localhost:5000/results/{x}.pdf"
+        with open("results/line_graph_file.png", "rb") as f:
+            line_graph = base64.b64encode(f.read()).decode('utf-8')
 
+        with open("results/weighted_graph_file.png", "rb") as f:
+            weighted_graph = base64.b64encode(f.read()).decode('utf-8')
+
+        with open("results/results.kml", "r") as f:
+            kml_contents = f.read()
+
+        # shutil.rmtree("results", ignore_errors=True)
+        shutil.rmtree("tables", ignore_errors=True)
+        shutil.rmtree("static/uploads", ignore_errors=True)
+
+        return render_template(
+            "report.html",
+            netcap_stats=netcap_stats,
+            email_addrs=email_addrs,
+            enum_images=enum_images,
+            packet_count=packet_count,
+            line_graph=line_graph,
+            weighted_graph=weighted_graph,
+            kml_contents=kml_contents
+        )
+    else:
+        return Response("Not Found", status=404)
 
 
 if __name__ == "__main__":
